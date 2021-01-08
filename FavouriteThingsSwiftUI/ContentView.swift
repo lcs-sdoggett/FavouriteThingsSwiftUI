9b40a3fee760a96ffa1bc69fe0faece0152e012e
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-08 15:10:19 -0500

    Picture and words added. Navigation title also changed.

--- FavouriteThingsSwiftUI/ContentView.swift
+++ FavouriteThingsSwiftUI/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            Image("basketball")
+                .padding()
+            
+            Text("I really enjoy watching playing and watching basketball")
+
+        }
+        .navigationTitle("Favourite Things")
     }
 }
 
